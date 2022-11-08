ddc3caf64a13ab0ceb73fd251d4a772691c68bea
s10jw <81946538+s10jw@users.noreply.github.com>
2022-11-07 18:25:45 -0800

    Introduced getNeighbors, which provides all neighboring grids and their info for any grid in question. Realized I can piggy-back one of the for loops in mergeGrids to achieve the same thing, will implement tomorrow.

--- GridShift.py
+++ GridShift.py
@@ -5,9 +5,16 @@ from matplotlib import pyplot as plt
 import sys
 import scipy.signal as signal
 import time
+from numba import jit
+from shutil import copyfile
 import os
 
 class GridShift:
+    thresh = [[-1, -1, -1], [-1, -1, 0], [-1, -1, 1], [-1, 0, -1], [-1, 0, 0], [-1, 0, 1], [-1, 1, -1], [-1, 1, 0],
+              [-1, 1, 1], [0, -1, -1], [0, -1, 0], [0, -1, 1], [0, 0, -1], [0, 0, 1], [0, 1, -1], [0, 1, 0],
+              [0, 1, 1], [1, -1, -1], [1, -1, 0], [1, -1, 1], [1, 0, -1], [1, 0, 0], [1, 0, 1], [1, 1, -1], [1, 1, 0],
+              [1, 1, 1]]
+
     def __init__(self, img, h):
         """
         Initializes GridShift instance, takes as input one BGR image as img, and a value h used to partition img into grid
@@ -22,29 +29,34 @@ class GridShift:
         self.S = {}
         self.C = {}
         self.H = {}
+        self.S_temp = self.S
+        self.C_temp = self.C
+        self.H_temp = self.H
         assert self.height % self.h == 0 and self.width % self.h == 0, 'Grid Cell size does not divide image.'
+
+    def getNeighbors(self, keys):
+        neighbors = {}
+        count, mulsum = None, None
+        for index in keys:
+            for i in GridShift.thresh:
+                val = np.add(index, i)
+                if val in keys:
+                    if index in neighbors.keys():
+                        mulsum += self.C_temp[val] * self.S_temp[val]
+                        count += self.C_temp
+                        neighbors[index][0].append(val)
+                        neighbors[index][1] += mulsum
+                        neighbors[index][2] += count
+                    else:
+                        mulsum = self.C_temp[val] * self.S_temp[val]
+                        count = self.C_temp[val]
+                        neighbors[index] = [[val], mulsum, count]
+        return neighbors
 
     def formGrids(self):
         raw_data = self.img.flatten()
-        y = 1
-        index = -1
-        hold = self.width / self.h
-        row = 0
         for i in range(len(raw_data)):
-            x = i + 1
-            # Creates index for an active grid of size h x h, commits all pixels with same index to the same dict.
-
-            if x % self.h + 1 == 0:
-                if x % self.width + 1 == 0:
-                    y += 1
-                    if y % self.h + 1 == 0:
-                        row += 1
-                        index = row * hold
-                    else:
-                        index = row * hold
-                else:
-                    index += 1
-
+            index = np.floor(np.divide(raw_data[i], self.h))
             if index in self.S.keys():
                 curr_sum = np.multiply(self.S[index], self.C[index])
                 new_sum = np.add(curr_sum, raw_data[i])
@@ -52,14 +64,23 @@ class GridShift:
 
                 self.S[index] = np.divide(new_sum, new_count)
                 self.C[index] = new_count
-                self.H[index].add(i)
+                self.H[index].append(i)
             else:
                 self.S[index] = raw_data[i]
                 self.C[index] = 1
-                self.H[index] = {i}
+                self.H[index] = [i]
 
     def mergeGrids(self):
-        while self.S.keys():
+        # Find initial neighbor info
+        neighbors = self.getNeighbors(self.S.keys())
+        # Create a temporary copy of hash contents
+        self.S_temp = self.S
+        self.C_temp = self.C
+        self.H_temp = self.H
+        # Empty parent hash tables
+        self.S = {}
+        self.C = {}
+        while neighbors.keys():
             # Create a temporary copy of hash contents
             S_temp = self.S
             C_temp = self.C
@@ -68,11 +89,8 @@ class GridShift:
             self.S = {}
             self.C = {}
             for i in S_temp.keys():
-                neighbor_sum = 0
-                neighbor_count = 0
-                for j in range(-1, 2):
-                    neighbor_sum += np.multiply(C_temp[i + j], S_temp[i + j])
-                    neighbor_count += C_temp[i + j]
+                neighbor_sum = neighbors[i][1] + S_temp[i] * C_temp[i]
+                neighbor_count = neighbors[i][2] + C_temp[i]
                 S_temp[i] = np.divide(neighbor_sum, neighbor_count)
                 index = np.floor(np.divide(S_temp[i], self.h))
                 if S_temp[index] in self.S.keys():
@@ -82,9 +100,10 @@ class GridShift:
 
                     self.S[index] = np.divide(new_sum, new_count)
                     self.C[index] = new_count
-                    self.H[index].add(i)
+                    self.H[index].append(i)
                 else:
                     self.S[index] = S_temp[i]
                     self.C[index] = C_temp[i]
                     self.H[index] = H_temp[i]
+                neighbors = self.getNeighbors(self.S.keys())
 
